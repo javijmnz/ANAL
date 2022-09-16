e46b0aa12ae0ddf493ddf593986899aa5f0f0649
javier <javier_jimenez2003@icloud.com>
2022-09-16 19:11:25 +0200

    Sesión 1 Laboratorio

--- practica1/permutations.c
+++ practica1/permutations.c
@@ -11,6 +11,10 @@
 
 
 #include "permutations.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
 
 /***************************************************/
 /* Function: random_num Date:                      */
@@ -27,7 +31,18 @@
 /***************************************************/
 int random_num(int inf, int sup)
 {
-  /* your code */
+  int k;
+  /* Comprobación de parámetros */
+  if (inf > sup)
+    return ERR;
+    
+  /* Mala implementación */
+  /* return inf + (rand() % (sup - inf + 1)); */
+  
+  /* Buena implementación */
+  k = inf + (int) ((sup - inf + 1.0) * (rand() / (RAND_MAX + 1.0)));
+  printf("%d", k);
+  return k;
 }
 
 /***************************************************/
@@ -45,8 +60,37 @@ int random_num(int inf, int sup)
 /***************************************************/
 int* generate_perm(int N)
 {
-  /* your code */
+  int *perm, i, j, aux;
+  printf("%d N", N);
+
+  /* Semilla */
+  srand(time(NULL));
+  
+  /* Reserva memoria para los números */
+  perm = (int *) calloc(N, sizeof(int));
+  if (!perm)
+    return NULL;
+  
+  /* Genera los números */
+  for (i = 1; i <= N; i++) {
+    perm[i] = i;
+    printf("%d", i);
+  }
+
+  /* Permuta los números */
+  for (i = 0; i <= N; i++) {
+    printf("%d %d \n", i, N);
+    j = random_num(0, N-1);
+    printf("Random num: %d", j);
+    aux = perm[i];
+    perm[i] = perm[j];
+    perm[j] = aux;
+    printf("Perm[%d]%d   Perm[%d]%d", i, perm[i], j, perm[j]);
+  }
+  
+  return perm;
 }
+
 
 /***************************************************/
 /* Function: generate_permutations Date:           */
@@ -63,7 +107,20 @@ int* generate_perm(int N)
 /* to each of the permutations                     */
 /* NULL en case of error                           */
 /***************************************************/
+/*
 int** generate_permutations(int n_perms, int N)
 {
-/* your code */
+  int**ret, i, flag = 0;
+
+  ret = calloc(n_perms, sizeof(int*));
+  if(!ret){
+    return NULL;        
+  }
+  
+  for(i = 0; i < n_perms; i++){
+    ret[i] = generate_perm(N);
+    if(ret[i] == NULL)
+  }
+
 }
+*/
