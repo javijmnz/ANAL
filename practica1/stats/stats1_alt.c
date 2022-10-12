fb476e3978e70d60c5a926aca162ce9881e04bd1
Cocoloco <pablofeliz2003@gmail.com>
2022-10-12 22:05:10 +0200

    stats1alt

--- practica1/stats/stats1_alt.c
+++ practica1/stats/stats1_alt.c
@@ -6,19 +6,19 @@
 
 int main(int argc, char** argv){
 
-    int inter, numInter, num, *array;
+    int inter, numInter, avg, *array;
     FILE *pf = NULL;
-    long long i, j, k, cont;
+    long long i, j, k;
 
     srand(time(NULL));
 
     if (argc != 7) {
     fprintf(stderr, "Input parameter error:\n\n");
-    fprintf(stderr, "%s -inter <int> -numInter <int> -numN <int> \n", argv[0]);
+    fprintf(stderr, "%s -inter <int> -numInter <int> -avg <int> \n", argv[0]);
     fprintf(stderr, "Where:\n");
     fprintf(stderr, " -inter : intervals.\n");
     fprintf(stderr, " -numInter : intervals.\n");
-    fprintf(stderr, " -numN : ammout of mumbers to generate.\n");
+    fprintf(stderr, " -avg : ammout of mumbers to generate per number.\n");
     exit(-1);
     }
 
@@ -26,11 +26,10 @@ int main(int argc, char** argv){
   for(i = 1; i < argc; i++) {
     if (strcmp(argv[i], "-inter") == 0) {
       inter = atoi(argv[++i]);
-      printf("%d",inter);
     } else if (strcmp(argv[i], "-numInter") == 0) {
       numInter = atoi(argv[++i]);
-    } else if (strcmp(argv[i], "-numN") == 0) {
-      num = atoi(argv[++i]);
+    } else if (strcmp(argv[i], "-avg") == 0) {
+      avg = atoi(argv[++i]);
     } else {
       fprintf(stderr, "Wrong Parameter %s \n", argv[i]);
     }
@@ -49,10 +48,9 @@ int main(int argc, char** argv){
         return ERR;
     }
     
-    cont = inter * numInter * 1000;
     for (j = 0; j < inter; j++){
         for(k = 0; k < numInter; k++){
-            for(i = 0; i < 100; i++){
+            for(i = 0; i < avg; i++){
                 array[random_num_mal(0, numInter * inter, RAND_MAX) / inter]++;
             }
         }
