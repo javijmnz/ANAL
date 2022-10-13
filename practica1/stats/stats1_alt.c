67f03359bfc6fc43784130127925cc1360238903
Cocoloco <pablofeliz2003@gmail.com>
2022-10-13 18:53:41 +0200

    cosa loca

--- practica1/stats/stats1_alt.c
+++ practica1/stats/stats1_alt.c
@@ -8,7 +8,7 @@ int main(int argc, char** argv){
 
     int inter, numInter, avg, *array;
     FILE *pf = NULL;
-    long long i, j, k;
+    int i, j, k;
 
     srand(time(NULL));
 
@@ -51,14 +51,14 @@ int main(int argc, char** argv){
     for (j = 0; j < inter; j++){
         for(k = 0; k < numInter; k++){
             for(i = 0; i < avg; i++){
-                array[random_num_mal(0, numInter * inter, RAND_MAX) / inter]++;
+                array[random_num(0, numInter * inter) / inter]++;
             }
         }
     }
     
 
     for(i = 0; i < numInter; i++){
-        fprintf(pf, "%lld %d\n", i*inter, array[i]);
+        fprintf(pf, "%d %d\n", i*inter, array[i]);
     }
 
     free(array);
