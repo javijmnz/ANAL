08efc5265a1019a6728847f69fd8f9b9aae27eab
javier <javier_jimenez2003@icloud.com>
2022-12-15 00:02:07 +0100

    añado comentarios y control de errores, y quito funciones que no usamos

--- practica3/permutations.h
+++ practica3/permutations.h
@@ -22,16 +22,8 @@
 typedef int *(* pfunc_perm)(int);
 
 int random_num(int inf, int sup);
-int random_num_mal(int inf, int sup);
+
 int* generate_perm(int N);
-int** generate_permutations(int n_perms, int N);
-int ** generate_permutations_alt(pfunc_perm func_perm, int n_perms, int N);
-int *generate_mergesort_worst_perm(int pot);
-int * generate_quicksort_worst_perm_v1(int N);
-int * generate_quicksort_worst_perm_v2(int N);
-int * generate_quicksort_worst_perm_v3(int N);
-int **copy_permutations(int **array, int n_perms, int N);
-int *copy_perm(int *array, int N);
 
 int *generate_sorted_perm(int N);
 
